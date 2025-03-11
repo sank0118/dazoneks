d9d9f3e2af361bd5551083a4afacc985243afac4
sank0118 <tks9901@naver.com>
2025-03-11 16:20:25 +0900

    react learned

---
+++ src/Item.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  data: string | number;
+  fn?: () => void;
+}
+
+const Item = ({ data, fn }: Props) => {
+  return (
+    <div>
+      Item:{data}
+      {fn && <button onClick={fn}>{data}</button>}
+    </div>
+  );
+};
+
+export default Item;
